a2a2bbb7224b5fcd291992dfdf76c10b6f2044cf
dgaie <dgaie@yahoo.fr>
2022-04-07 12:49:09 +0200

    test

--- linux_scripts/update.sh
+++ linux_scripts/update.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+#First installation
+
 sudo apt update -y
 sudo apt upgrade -y
+
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt update -y
+
+sudo apt install python3 python3-pip -y
+sudo apt install ansible -y
+
+
+
+
+
 sudo apt autoremove -y
+
+clear
+python3 --version
+pip3 --version
+ansible --version
+
